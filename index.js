f97e975121dafcab34158c59769b83fde1ecb159
webdmamun <73527253+webdmamun@users.noreply.github.com>
2021-10-31 02:54:58 +0600

    post api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
-// const ObjectId = require("mongodb").ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 
 const cors = require("cors");
 require("dotenv").config();
@@ -24,6 +24,9 @@ async function run() {
     await client.connect();
     const database = client.db("toorDB");
     const servicesCollection = database.collection("services");
+    const servicesBookCollection = database.collection("bookedservice");
+    const hotelsCollection = database.collection("hotels");
+    const bookedhotelCollection = database.collection("bookedhotel");
 
     //Get API
     app.get("/services", async (req, res) => {
@@ -31,23 +34,80 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+    app.get("/hotels", async (req, res) => {
+      const cursor = hotelsCollection.find({});
+      const hotels = await cursor.toArray();
+      res.send(hotels);
+    });
+
+    // get booked services
+    app.get("/bookedservice", async (req, res) => {
+      const cursor = servicesBookCollection.find({});
+      const bookedServices = await cursor.toArray();
+      res.send(bookedServices);
+    });
+
+    // get booked hotels
+    app.get("/bookedhotel", async (req, res) => {
+      const cursor = bookedhotelCollection.find({});
+      const bookedHotel = await cursor.toArray();
+      res.send(bookedHotel);
+    });
+
+    // add booked services
+    app.post("/bookedservice", async (req, res) => {
+      const bookedService = req.body;
+      const result = await servicesBookCollection.insertOne(bookedService);
+      res.json(result);
+    });
+    // add booked hotel
+    app.post("/bookedhotel", async (req, res) => {
+      const bookedHotel = req.body;
+      const result = await bookedhotelCollection.insertOne(bookedHotel);
+      res.json(result);
+    });
 
     // Get Single Service
-    // app.get("/services/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const service = await servicesCollection.findOne(query);
-    //   res.json(service);
-    // });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+    app.get("/bookedservice/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesBookCollection.findOne(query);
+      res.json(service);
+    });
+    // Get Single hotel
+    app.get("/hotels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const hotels = await hotelsCollection.findOne(query);
+      res.json(hotels);
 
-    //Post API
-    // app.post("/services", async (req, res) => {
-    //   const service = req.body;
-    //   console.log("hit the post api", service);
-    //   const result = await servicesCollection.insertOne(service);
-    //   console.log(result);
-    //   res.json(result);
-    // });
+      app.get("/bookedhotel/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const bookedHotel = await bookedhotelCollection.findOne(query);
+        res.json(bookedHotel);
+      });
+
+      // Delete Booked Service
+      app.delete("/bookedservice/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await servicesBookCollection.deleteOne(query);
+        res.json(result);
+      });
+
+      app.delete("/bookedhotel/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log("deleting user with id", req);
+        res.json(1);
+      });
+    });
   } finally {
     // await client.close();
   }
